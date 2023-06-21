c0b573cc41e7a4905eb327d678efeac2d9112427
daryavolnaya <129992395+daryavolnaya@users.noreply.github.com>
2023-06-21 13:34:28 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         RainCheck Weather app
+
       </header>
       <footer>
         This project was coded by <a href="https://github.com/daryavolnaya" target="_blank" rel="noreferrer">Daria Romaniuk</a>, and it is <a href="https://github.com/daryavolnaya/raincheck-react" target="_blank" rel="noreferrer">open-sourced</a> 
       </footer>
+      </div>
     </div>
   );
 }
