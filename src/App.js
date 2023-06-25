1dacc035225fa3bb8546ba0b8f675691242973bc
daryavolnaya <129992395+daryavolnaya@users.noreply.github.com>
2023-06-25 15:23:20 +0200

    Added API integration

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ export default function App() {
     <div className="App">
       
       <div className="container">
-        <div className="wrapper">
+     
       <header className="App-header">
       </header>
-      <Weather/>
+      <Weather defaultCity="Corfu"/>
       <footer>
         This project was coded by <a href="https://github.com/daryavolnaya" target="_blank" rel="noreferrer">Daria Romaniuk</a>, and it is <a href="https://github.com/daryavolnaya/raincheck-react" target="_blank" rel="noreferrer">open-sourced</a> 
       </footer>
-      </div>
+      
       </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 
 import "./Weather.css";
 
-export default function Weather (){
+export default function Weather (props){
+const [weatherData, setWeatherData] = useState({ready:false});
+  function handleResponse(response){
+    console.log(response.data);
+    setWeatherData({
+      ready:true,
+      temperature: Math.round(response.data.main.temp),
+      feelsLike: Math.round(response.data.main.feels_like),
+      humidity: response.data.main.humidity,
+      wind: Math.round(response.data.wind.speed),
+      city: response.data.name,
+      description: response.data.weather[0].description,
+      time: "14:24",
+      weekday: "Sun",
+      date: "JUN 21",
+    })
+  }
+
+  if (weatherData.ready){
 	return (<div className="Weather">
     <form className="mb-2">
       <div className="row">
         <div className="col-9">
-    <input type="search" placeholder="enter a city..." className="form-control"></input>
+    <input type="search" placeholder="enter a city..." className="form-control" autoFocus="on"></input>
    </div>
     <div className="col-3">
-    <input type="submit" value="Search" className="btn btn-dark px-md-5"/>
+    <input type="submit" value="Search" className="btn btn-dark w-100"/>
     </div>
     </div>
 </form>
@@ -18,13 +37,13 @@ export default function Weather (){
       <div className="col-3">
         <ul>
           <li className="temperature">
-        29ºC
+        {weatherData.temperature}ºC
         </li>
          <li>
-        Sun / JUN 21
+        {weatherData.weekday} / {weatherData.date}
         </li>
          <li>
-        14:24
+        {weatherData.time}
         </li>
         </ul>
         </div>
@@ -33,22 +52,29 @@ export default function Weather (){
           <li>
              <img src="https://static-00.iconduck.com/assets.00/partly-cloudy-icon-2048x2017-zr2ino3n.png" alt="partly cloudy"/>
         </li>
-        <li className="description">
-        Cloudy
+        <li className="description text-capitalize">
+        {weatherData.description}
         </li>
         </ul>
         </div>
     </div>
     <div className="row main-content">
       <div className="col-7">
-    <h2>📍 Brooklyn, USA</h2>
+    <h2>📍 {weatherData.city}</h2>
     <ul className="current-weather">
-      <li>Humidity: 67%</li>
-      <li>Chance of rain: 11%</li>
-      <li>Wind 2 m/s</li>
-      <li>Feels like 27ºC</li>
+      <li>Humidity: {weatherData.humidity}%</li>
+      <li>Wind {weatherData.wind} m/s</li>
+      <li>Feels like {weatherData.feelsLike}ºC</li>
     </ul>
     </div>
 	</div>
     </div>);
+  } else {
+
+  let apiKey = "96ad27349a64ea1dcdfbe6f4d458c085";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+  return "Loading..."
+}
+
 }
