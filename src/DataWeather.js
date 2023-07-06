fc48d3d705cb23d5d56bc3f9ea8c1ccb3e78855f
daryavolnaya <129992395+daryavolnaya@users.noreply.github.com>
2023-07-06 15:57:29 +0200

    removed the conversion to fahrenheit

--- src/DataWeather.js
+++ src/DataWeather.js
@@ -1,6 +1,5 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
-import WeatherTemperature from "./WeatherTemperature";
 import Forecast from "./Forecast";
 import HourlyForecast from "./HourlyForecast";
 
@@ -9,12 +8,14 @@ export default function DataWeather(props){
         <div className="row above">
       <div className="col-2 weather">
         <ul>
-          <WeatherTemperature celsius={props.data.temperature}/>
+         <span className="temperature"> {Math.round(props.data.temperature)}
+         <span className="celsius">ºC</span>
+         </span>
         <FormattedDate date={props.data.date}/>
        
         </ul>
         </div>
-      <div className="col-2 icon"> 
+      <div className="col-2 main-icon"> 
       <ul>
           <li>
              <img src={props.data.iconUrl} alt={props.data.description} className="mainIcon"/>
@@ -25,11 +26,12 @@ export default function DataWeather(props){
         </ul>
          
         </div>
+      
         <div className="col-8"><HourlyForecast coordinates={props.data.coordinates} /></div>
         
     </div>
     <div className="row main-content">
-      <div className="col m-lg-5">
+      <div className="col m-lg-5 cityWeather">
     <h2 className="city">📍 {props.data.city}</h2>
     <ul className="current-weather">
       <li>Humidity: {props.data.humidity}%</li>
